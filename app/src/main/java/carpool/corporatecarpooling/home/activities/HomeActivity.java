c4edc62ca501274226d147957c7aa6df61a1db0c
ShalkiBhargava <bhargava.shalki@gmail.com>
2017-07-25 11:43:39 +0200

    Changes in AndroidManifest.xml to make the main activity as the launch page and disabling auto rotation in login and registration pages.

--- app/src/main/java/carpool/corporatecarpooling/home/activities/HomeActivity.java
+++ app/src/main/java/carpool/corporatecarpooling/home/activities/HomeActivity.java
@@ -13,7 +13,6 @@ public class HomeActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-       // MainController.launchActivityPage(R.layout.activity_home);
         setContentView(R.layout.activity_home);
          }
 
